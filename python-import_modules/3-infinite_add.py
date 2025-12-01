c290d8d1c61aeac591bb47519377a86cefda6960
Nihad-Suleyman <nihad.suleymanov007@gmail.com>
2025-12-01 09:45:12 +0000

    done

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+import sys
 cnt = 0
 if __name__ == "__main__":
-    while int(input()):
-        cnt += n
+    for i in range(0, len(sys.argv)):
+        cnt += int(sys.argv[i])
     print("{}".format(cnt))
