5e1d551b1b35937e9e685483828cc74523f282c1
Nihad-Suleyman <nihad.suleymanov007@gmail.com>
2025-12-01 09:23:16 +0000

    now what

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 import sys
-if __name__ == "__main__": 
+if __name__ == "__main__":
     count = len(sys.argv) - 1
     if count == 0:
         print("0 arguments.")
     elif count == 1:
         print("1 argument:")
-    else: 
+    else:
         print("{} arguments:".format(count))
 
     for i in range(1, len(sys.argv)):
