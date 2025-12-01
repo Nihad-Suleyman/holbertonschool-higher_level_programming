09de72f3e7d71b046340cc70e0ed816f0f085829
Nihad-Suleyman <nihad.suleymanov007@gmail.com>
2025-12-01 09:17:26 +0000

    now done

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
         print("{} arguments:".format(count))
 
     for i in range(1, len(argv)):
-        print("{}: {}".format(i, sys.argv[i])
+        print("{}: {}".format(i, sys.argv[i]))
