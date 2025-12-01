dc65944105a43a7a77425fd79bd05c2c518470fb
Nihad-Suleyman <nihad.suleymanov007@gmail.com>
2025-12-01 09:19:07 +0000

    should work now

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__": 
-    count = len(sys.argv) - 1
+    count = len(argv) - 1
     if count == 0:
         print("0 arguments.")
     elif count == 1:
@@ -9,4 +9,4 @@ if __name__ == "__main__":
         print("{} arguments:".format(count))
 
     for i in range(1, len(argv)):
-        print("{}: {}".format(i, sys.argv[i]))
+        print("{}: {}".format(i, argv[i]))
