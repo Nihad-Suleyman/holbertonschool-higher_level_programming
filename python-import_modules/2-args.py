96dc610840672147a1d6cf7e5f3fad4a932b3b63
Nihad-Suleyman <nihad.suleymanov007@gmail.com>
2025-12-01 09:16:18 +0000

    nod wone

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__": 
-    for i in range(len(argv)):
-        print("{}: {}".format(i + 1, argv[i])
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else: 
+        print("{} arguments:".format(count))
+
+    for i in range(1, len(argv)):
+        print("{}: {}".format(i, sys.argv[i])
