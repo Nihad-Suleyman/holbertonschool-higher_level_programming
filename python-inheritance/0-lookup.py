0e5fae552dcb64cd169ea085b1cf8d655074be00
Nihad-Suleyman <nihad.suleymanov007@gmail.com>
2025-12-10 15:48:39 +0000

    done

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""we will create a function of simple list for task 1"""
+
+def lookup(obj):
+    return []
---
+++ python-inheritance/README.md
@@ -0,0 +1,1 @@
+now let's move on with inheritance, let's see what we will be able to do
