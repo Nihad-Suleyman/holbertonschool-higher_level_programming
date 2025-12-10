1a91c9148435d87b8b8d6014d0ffd1b3c34e580c
nihad <Nihad>
2025-12-10 17:29:45 +0000

    done

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""done"""
+
+
+class BaseGeometry:
+    """class"""
+
+    def area(self):
+        """raises exception"""
+        raise Exception("area() is not implemented")
