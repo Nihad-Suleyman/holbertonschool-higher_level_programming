9a502b0536d8d26ab2fb6b5a6ebe09c899dac17e
Nihad <nihad.suleymanov007@gmail.com>
2025-12-14 15:57:00 +0000

    done

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module that defines class_to_json function"""
+
+
+def class_to_json(obj):
+    """Returns the dictionary description for JSON serialization."""
+    return obj.__dict__
