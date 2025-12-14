700c802b54876ebc723974e8e71e6dee1904b6c8
Nihad <nihad.suleymanov007@gmail.com>
2025-12-14 15:36:26 +0000

    done

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module that defines append_write function"""
+
+
+def append_write(filename="", text=""):
+    """Appends a string to a text file (UTF8) and returns number of character"""
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
