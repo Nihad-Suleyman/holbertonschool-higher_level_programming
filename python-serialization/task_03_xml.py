ca0265b735fba230ff567128f44bc852ac74c75e
Nihad <nihad.suleymanov007@gmail.com>
2025-12-15 10:28:45 +0000

    done

---
+++ python-serialization/task_03_xml.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""let's now work on XML a bit"""
+
+
+import xml.etree.ElementTree as ET
+
+def serialize_to_xml(dictionary, filename):
+    root = ET.Element("data")
+    for key, value in dictionary.items():
+        child = ET.Element(root, key)
+        child.set("type", type(value).__name__)
+        child.text = value
+
+    tree = ET.ElementTree(root)
+    tree.write(filename, encoding='utf-8', xml_declaration=False)
+
+def deserialize_from_xml(filename):
+    tree = ET.parse(filename)
+    root = tree.getroot()
+    s = {}
+    for child in root:
+        s[child.tag] = child.text
+    return s
