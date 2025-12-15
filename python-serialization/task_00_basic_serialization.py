fa900f26c1a89c6fd6dbde1c2034efb6fdeef7d2
Nihad <nihad.suleymanov007@gmail.com>
2025-12-15 09:15:27 +0000

    done

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -7,4 +7,4 @@ def serialize_and_save_to_file(data, filename):
     filename = json.dumps(data).encode("utf-8")
 
 def load_and_deserialize(filename):
-    filename = json.loads(filename.decode("utf-8")
+    filename = json.loads(filename.decode("utf-8"))
