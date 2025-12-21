6f8117c29fc5550150b6d0b957a4b1207825c3c2
NIhad <nihad>
2025-12-21 10:50:04 +0000

    done

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- let's now work on second one
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
