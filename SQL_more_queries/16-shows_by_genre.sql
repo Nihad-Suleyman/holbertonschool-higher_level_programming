2a5be0116994289a2017d3937f968bf996b5f525
NIhad <nihad>
2025-12-21 13:49:26 +0000

    done

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- now let's do the last one
+
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title, tv_genres.name;
